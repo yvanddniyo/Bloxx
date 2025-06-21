49fd158072eddea1c2e33f0b0803d3905a5ea216
yvanddniyo <nyvan80076@gmail.com>
2025-06-21 12:59:32 +0200

    Fixing the simple version crud

--- src/articles/articles.service.ts
+++ src/articles/articles.service.ts
@@ -7,26 +7,47 @@ import { PrismaService } from 'src/prisma/prisma.service';
 export class ArticlesService {
   constructor(private prisma: PrismaService) {}
   create(createArticleDto: CreateArticleDto) {
-    return 'This action adds a new article';
+    return this.prisma.article.create({
+      data: createArticleDto,
+    });
+  }
+
+  findDrafts() {
+    return this.prisma.article.findMany({
+      where: {
+        published: false,
+      },
+    });
   }
 
   findAll() {
     return this.prisma.article.findMany({
       where: {
-        published: true
-      }
+        published: true,
+      },
     });
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} article`;
+    return this.prisma.article.findUnique({
+      where: {
+        id,
+      },
+    });
   }
 
   update(id: number, updateArticleDto: UpdateArticleDto) {
-    return `This action updates a #${id} article`;
+    return this.prisma.article.update({
+      where: {
+        id,
+      },
+      data: updateArticleDto,
+    });
   }
 
   remove(id: number) {
-    return `This action removes a #${id} article`;
+    return this.prisma.article.delete({
+      where: { id },
+    });
   }
 }
--- src/articles/entities/article.entity.ts
+++ src/articles/entities/article.entity.ts
@@ -1,1 +1,23 @@
-export class Article {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Article } from '@prisma/client';
+
+export class ArticleEntity implements Article {
+  published: boolean;
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  title: string;
+
+  @ApiProperty({ required: false, nullable: true })
+  description: string | null;
+
+  @ApiProperty()
+  body: string;
+
+  @ApiProperty()
+  createAte: Date;
+
+  @ApiProperty()
+  updatedAt: Date;
+}

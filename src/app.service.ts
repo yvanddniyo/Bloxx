f5933b77bfd78255a18ffeeb32d8e09c3bd735d6
yvanddniyo <nyvan80076@gmail.com>
2025-06-22 12:48:44 +0200

    Changing the title of the welcome message of the backend app.

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello Developer, This is the backend of the bloxx app.! ';
   }
 }

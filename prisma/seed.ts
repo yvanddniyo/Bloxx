1a0383cd97d5bc1b58ca1c0409189166b3cdc970
yvanddniyo <nyvan80076@gmail.com>
2025-06-28 15:31:31 +0200

    Fixng prismaClient issue

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../node_modules/@prisma/client';
+import { PrismaClient } from '../generated/prisma/client';
 
 const prisma = new PrismaClient();
 async function main() {

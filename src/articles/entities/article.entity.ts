6db1c57f826b9e86320111f7e3c4ba5faeb429a0
yvanddniyo <nyvan80076@gmail.com>
2025-06-28 15:35:24 +0200

    Fixng prismaClient issue

--- src/articles/entities/article.entity.ts
+++ src/articles/entities/article.entity.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Article } from 'generated/prisma';
+import { Article } from 'generated/prisma/client';
 
 export class ArticleEntity implements Article {
   published: boolean;
